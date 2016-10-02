dee619d03a6eaf58195fac470d88ed8acf209507
Levin Nickelsen <lnickelsen@zuse.local>
2016-10-02 15:25:42 +0200

    update on awspysetup.sh

--- awspysetup.sh
+++ awspysetup.sh
@@ -1,13 +1,27 @@
 # installing the necessary packages for python 3.5 and their libraries
 sudo yum install python35 python35-virtualenv python35-pip
 sudo yum install gcc gcc-c++ gcc-gfortran
-sudo yum install numpy scipy python-matplotlib ipython python-pandas sympy python-nose atlas-devel #
- install libraries v2.7, seems to be necessary for scipy version 3.5 from pip..
+sudo yum install lapack-devel blas-devel atlas-devel 
+sudo yum install libpng-devel
+#sudo yum install numpy scipy python-matplotlib ipython python-pandas sympy python-nose atlas-devel # install libraries v2.7, seems to be necessary for scipy version 3.5 from pip..
+
+# before installing scipy, create swap memory
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
+
+sudo pip-3.5 install scipy
+
+sudo swapoff /var/swap.1
+sudo rm /var/swap.1
 
 # installing libraries for python 3.5
 sudo pip-3.5 install numpy # works, but takes a while
 sudo pip-3.5 install pandas # works (after installing gcc-c++)
-sudo pip-3.5 install scipy # works after installing the 2.7 scipy pack via sudo yum
-sudo pip-3.5 install matplotlib 
-sudo pip-3.5 install sklearn # requires scipy
+sudo pip-3.5 install matplotlib   
+sudo pip-3.5 install sklearn 
+sudo pip-3.5 install scikit-learn 
+sudo pip-3.5 install sympy
+sudo pip-3.5 install pillow
 sudo pip-3.5 install os
+sudo pip-3.5 install twitter
--- bosch.py
+++ bosch.py
@@ -17,10 +17,10 @@ df = pd.read_csv('train_numeric.csv', nrows=100000)
 #df.describe
 
 target = df[['Response']]
-target.describe()
+#target.describe()
 
 train = df.ix[:,0:969]
-train.isnull().sum()
+#train.isnull().sum()
 
 # create imputer to replace missing values with the mean or median
 # replace later by a more sophisticated imputation approach
@@ -34,7 +34,7 @@ rf.fit(train, target)
 
 # read test data and also impute
 # need to do the loop because of memory overload of my laptop
-for ii in range(0,9):
+for ii in range(0,12):
     test = pd.read_csv('test_numeric.csv',nrows=100000, skiprows=ii*100000)
     test = imp.fit_transform(test)
     testid=test[:,0]
